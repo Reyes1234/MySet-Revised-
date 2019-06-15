e266a605937bd098d2353947462ae93878759aef
Reyes1234 <50181399+Reyes1234@users.noreply.github.com>
2019-06-14 23:20:45 -0400

    Update mysets.js
    New updates, there are no red errors. Now how do you make it do what you want it to do?

--- mysets.js
+++ mysets.js
@@ -6,33 +6,33 @@ class Sets{
     this.has = has;
   }
   isEqual(other_set){
-   console.log('this set' + this.add + 'contains the same elements');
+   console.log('this set ' + this.add + ' contains the same elements');
   }
   toArray(){
-    console.log('this set' + this.remove + 'is a copy of this array');
+    console.log('this set ' + this.remove + ' is a copy of this array');
 
   }
   forEach(callback){
-    console.log('this set' + this.size + 'executes each element');
+    console.log('this set ' + this.size + ' executes each element');
   }
   clone(){
-    console.log('this set' + this.has + 'is an indepent object with the same data');
+    console.log('this set ' + this.has + ' is an indepent object with the same data');
   }
 }
 //no errors so far
 
-let firstset = new Set([1,2,3]);
+let firstset = new Sets([1,2,3]);
 console.log(firstset);
-let other_set = new Set([1,4,5]);
+let other_set = new Sets([1,4,5]);
 firstset.isEqual(other_set);
 
-let thirdset = new Set ([apple, banana, grapes]);
-let fourthset = new Set ([apple, 1, 2, 4]);
+let thirdset = new Sets (["apple", "banana", "grapes"]);
+let fourthset = new Sets (["apple", 1, 2, 4]);
 thirdset.toArray();
 
-let fifthset = new Set ([5,6,7]);
-fifthset.forEach(callback);
+let fifthset = new Sets ([5,6,7]);
+fifthset.forEach();
 
-let sixthset = new Set ([Amy, Fred, James]);
-let seventhset = new Set ([Amy, Fred, John]);
+let sixthset = new Sets (["Amy", "Fred", "James"]);
+let seventhset = new Sets (["Amy", "Fred", "John"]);
 sixthset.clone();
